2ca0d7986cc65a59521fe5b573a99618ceb01d56
thanhbtm1 <trancaothanhbtm9@gmail.com>
2026-06-18 22:19:27 +0700

    [Thanh/18/6] fix bug

--- include/bundle.h
+++ include/bundle.h
@@ -3,9 +3,10 @@
 
 #include "types.h"
 
-int create_bundle(Bundle bundles[], const int *count, const Bundle *new_bundle);
+int create_bundle(Bundle bundles[], int *count, const Bundle *new_bundle);
 int add_product_to_bundle(Bundle *bundle, int product_id);
-int remove_product_from_bundle(Bundle *bundle, int product_id);
+int remove_product_from_bundle(Bundle bundles[], int *bundle_count,
+                               Bundle *bundle, int product_id);
 float calculate_bundle_price(const Bundle *bundle, const Product products[],
                              int product_count);
 int get_virtual_bundle_stock(const Bundle *bundle, const Product products[],
